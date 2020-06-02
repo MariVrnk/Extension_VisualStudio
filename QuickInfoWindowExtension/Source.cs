42711f9b8a49a6022a12589f3eec43a68968c407
Мария Воронько <mari.voronko135@gmail.com>
2020-06-02 23:23:57 +0300

    First commit

---
+++ QuickInfoWindowExtension/Source.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace QuickInfoWindowExtension
+{
+    internal class Source : IQuickInfoSource
+    {
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> quickInfoContent, out ITrackingSpan applicableToSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class SourceProvider : IQuickInfoSourceProvider
+    {
+        public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Controller : IIntellisenseController
+    {
+        public void ConnectSubjectBuffer(ITextBuffer subjectBuffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Detach(ITextView textView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ControllerProvider : IIntellisenseControllerProvider
+    {
+        public IIntellisenseController TryCreateIntellisenseController(ITextView textView, IList<ITextBuffer> subjectBuffers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
