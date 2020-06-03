4a3f209120cf40a8fb34e2749a68dae17a958bcf
Мария Воронько <mari.voronko135@gmail.com>
2020-06-03 23:36:31 +0300

    MouseHover

--- QuickInfoWindowExtension/Source.cs
+++ QuickInfoWindowExtension/Source.cs
@@ -65,21 +65,41 @@ namespace QuickInfoWindowExtension
             m_textView = textView;
             m_subjectBuffers = subjectBuffers;
             m_provider = provider;
+            m_textView.MouseHover += this.OnTextViewMouseHover;
         }
-        public void ConnectSubjectBuffer(ITextBuffer subjectBuffer)
-        {
-            throw new NotImplementedException();
+
+        private void OnTextViewMouseHover(object sender, MouseHoverEventArgs e)
+        {            
+            SnapshotPoint? point = m_textView.BufferGraph.MapDownToFirstMatch
+                 (new SnapshotPoint(m_textView.TextSnapshot, e.Position),
+                PointTrackingMode.Positive,
+                snapshot => m_subjectBuffers.Contains(snapshot.TextBuffer),
+                PositionAffinity.Predecessor);
+
+            if (point != null)
+            {
+                ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(point.Value.Position,
+                PointTrackingMode.Positive);
+
+                if (!m_provider.QuickInfoBroker.IsQuickInfoActive(m_textView))
+                {
+                    m_session = m_provider.QuickInfoBroker.TriggerQuickInfo(m_textView, triggerPoint, true);
+                }
+            }
         }
 
         public void Detach(ITextView textView)
         {
-            throw new NotImplementedException();
+            if (m_textView == textView)
+            {
+                m_textView.MouseHover -= this.OnTextViewMouseHover;
+                m_textView = null;
+            }
         }
 
-        public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer)
-        {
-            throw new NotImplementedException();
-        }
+        public void ConnectSubjectBuffer(ITextBuffer subjectBuffer) { }
+
+        public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer) { }
     }
 
     [Export(typeof(IIntellisenseControllerProvider))]
